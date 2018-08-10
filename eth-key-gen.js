d31b9aeb73d7f49d5224a08d333fa45ddedaef5b
Greg Kapka <greg@kapka.co.uk>
2018-08-10 10:07:27 +0100

    make neater with compose

--- eth-key-gen.js
+++ eth-key-gen.js
@@ -1,25 +1,28 @@
-const Task       = require('data.task')
-    , keythereum = require('keythereum')
-    , ethutil    = require('ethereumjs-util')
-    , {compose, map} = require('ramda')
+const {compose}          = require('ramda')
+const Task               = require('data.task')
+    , keythereum         = require('keythereum')
+    , {privateToAddress} = require('ethereumjs-util')
 
-//    privateKeyToAddress :: string -> string
-const privateKeyToAddress = privateKey =>
-  `0x${ethutil.privateToAddress(hexToBytes(privateKey)).toString('hex')}`
+//    stringFromHex :: hexString -> string
+const stringFromHex = hex =>
+  `0x${hex.toString('hex')}`
 
 //    hexToBytes :: string -> [bytes]
 const hexToBytes = hex => 
   new Array (hex.length/2).fill().map((_, i) => parseInt(hex.substr(i * 2, 2), 16))
 
+  //    privateKeyToAddress :: string -> string
+const privateKeyToAddress = compose(stringFromHex, privateToAddress, hexToBytes)
+
 //    createKey :: () -> Task error {key}
-const createKey = () => 
+const createKey = _ => 
   new Task ((_, res) => res(keythereum.create()))
 
-//    keyObject = string -> {string} -> {string}
-const keyObject = (password, ck) =>
-  keythereum.dump(password, ck.privateKey, ck.salt, ck.iv)
+//    keyObject = string -> {key} -> {key}
+const keyObject = (password, k) =>
+  keythereum.dump(password, k.privateKey, k.salt, k.iv)
 
-//    getPrivKey :: string -> {string} -> string
+//    getPrivKey :: string -> {key} -> string
 const getPrivKey = (password, keyObj) =>
   keythereum.recover(password, keyObj).toString('hex')
 
@@ -28,17 +31,11 @@ const generatePrivKey = password =>
   createKey()
     .map(k => keyObject(password, k))
     .map(k => getPrivKey(password, k))
-//   compose(map(getPrivKey(password)), map(keyObject(password)), createKey)
 
-//    logKeyPairs :: string -> Task error ethAddress
-const logKeyPairs = password =>
+// IMPURE! module.exports :: string -> ethereum key pair
+module.exports = password => 
   generatePrivKey(password)
     .map(m => console.log(`Private key: ${m}`) || m)
     .map(privateKeyToAddress)
-
-// IMPURE!
-// module.exports :: string -> ethereum key pair
-module.exports = password => 
-  logKeyPairs(password)
     .fork(e => console.log(`Error ${e}`),
           r => console.log(`Public address: ${r}`))
